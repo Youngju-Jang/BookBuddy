228a2d9682ffdd2b736776c61d9a1a91a1658369
hippo <joj1043@nate.com>
2022-09-16 16:54:27 +0900

    section7_1 자동클래스 탐지로 빈등록

--- src/main/java/moviebuddy/MovieBuddyFactory.java
+++ src/main/java/moviebuddy/MovieBuddyFactory.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.*;
 // Import >> 다른 클래스에서 빈구성정보를 불러오기위해 사용함
 //@ImportResource("xml file location") // >> 빈 구성정보 불러올 수 있음
 @Configuration
+@ComponentScan(basePackages = {"moviebuddy"}) // 자동으로 csvMovieReader찾게할거임
 @Import({MovieBuddyFactory.DomainModuleConfig.class, MovieBuddyFactory.DataSourceModuleConfig.class})
 public class MovieBuddyFactory {
     @Configuration
@@ -23,9 +24,6 @@ public class MovieBuddyFactory {
 
     @Configuration
     static class DataSourceModuleConfig {
-        @Bean
-        public MovieReader movieReader(){
-            return new CsvMovieReader();
-        }
+       
     }
 }
